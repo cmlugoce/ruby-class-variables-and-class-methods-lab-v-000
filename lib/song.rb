1de793c1ecf735990da9da3f7e6dd4c77a38d138
Cristina Lugo <cmlugoce@syr.edu>
2018-06-14 19:24:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   @@genre = []
   @@artist_count = {} 
   @@genre_count = {}
-attr_accessor :artist, :genre, :name  
+attr_accessor :name, :artist, :genre  
   
   def initialize(name, artist, genre)
     @name = name
